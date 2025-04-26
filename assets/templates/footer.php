eab5b116ce792150f7822ea8efe259733a4550cc
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-04-26 21:21:55 +0200

    chore(templates\footer): edit structural code

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -27,10 +27,12 @@
     <div class="container">
         <div class="grid gap-12 grid-cols-1 md:grid-cols-12 mb-20">
             <div class="md:col-span-4 flex flex-col items-center md:items-start text-center md:text-left">
-                <a href="/"><div class="flex items-center mb-2">
-                    <img src="assets/img/logoGitpedia.png" alt="Logo Git" class="mx-3" />
-                    <h5 class="text-2xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Gitopedia</h5>
-                </div></a>
+                <a href="/">
+                    <div class="flex items-center mb-2">
+                        <img src="assets/img/logoGitpedia.png" alt="Logo Git" class="mx-3" />
+                        <h5 class="text-2xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Gitopedia</h5>
+                    </div>
+                </a>
                 <p>Parce que git <code>`commit -m 'help'`</code> ne fonctionne pas.</p>
             </div>
 
