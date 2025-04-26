b1b5534e4a9a9d7ee1400401b74a8ed7074e9b07
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-04-26 21:08:04 +0200

    feat(templates): add header and footer
    style(templates\footer): add hover effect to footer text
    
    feat(templates\footer): add link logo
    
    chore(templates\footer): edit structural code
    
    feat(templates\header): add link logo
    
    fix(templates/header): flex navbar
    
    style(templates\header): add gap logo
    
    style(templates\header): add color body
    
    chore(cleanup): remove an useless file, image and clear the css
    
    feat(templates\header): add a 12-column template to the entire page, edit the buttons and display the logo
    
    feat(templates\header): edit the text, change the links path and display the logo
    
    feat(home\index): add animation header and footer to the buttons and logos

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -23,14 +23,16 @@
             Le fond sombre (bg-dark) et le texte blanc (text-white) créent
             un contraste visuel avec le reste de la page.
         -->
-<footer class="bg-[#303034] flex justify-center text-white mt-5 pt-10 pb-5 px-4 sm:px-6 lg:px-8">
-    <div class="container">
+<footer class="col-span-12 bg-[#303034] text-white pt-10 pb-5">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
         <div class="grid gap-12 grid-cols-1 md:grid-cols-12 mb-20">
             <div class="md:col-span-4 flex flex-col items-center md:items-start text-center md:text-left">
-                <div class="flex items-center mb-2">
-                    <img src="assets/img/logoGitpedia.png" alt="Logo Gitopedia" class="mx-3" />
-                    <h5 class="text-2xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Gitopedia</h5>
-                </div>
+                <a href="/">
+                    <div class="flex items-center mb-2 transition-all duration-200 transform hover:scale-[1.03] active:scale-[0.95]">
+                        <img src="assets/img/logoGit.png" alt="Logo Git" class="mx-3" />
+                        <h5 class="text-2xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Gitopedia</h5>
+                    </div>
+                </a>
                 <p>Parce que git <code>`commit -m 'help'`</code> ne fonctionne pas.</p>
             </div>
 
@@ -38,40 +40,40 @@
                 <div class="flex flex-col gap-2">
                     <h5 class="text-xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Ressources</h5>
                     <ul class="space-y-2">
-                        <li><a href="/" class="text-white no-underline hover:underline">Accueil</a></li>
-                        <li><a href="/actualite" class="text-white no-underline hover:underline">Actualité</a></li>
-                        <li><a href="/documentation" class="text-white no-underline hover:underline">Documentation</a></li>
-                        <li><a href="/contact" class="text-white no-underline hover:underline">À propos</a></li>
+                        <li><a href="/" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Accueil</a></li>
+                        <li><a href="/news" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Actualité</a></li>
+                        <li><a href="/documentation" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Documentation</a></li>
+                        <li><a href="/contact" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">À propos</a></li>
                     </ul>
                 </div>
 
                 <div class="flex flex-col gap-2">
                     <h5 class="text-xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Niveaux</h5>
                     <ul class="space-y-2">
-                        <li><a href="/debutant" class="text-white no-underline hover:underline">Débutant</a></li>
-                        <li><a href="/intermediaire" class="text-white no-underline hover:underline">Intermédiaire</a></li>
-                        <li><a href="/avance" class="text-white no-underline hover:underline">Avancé</a></li>
-                        <li><a href="/expert" class="text-white no-underline hover:underline">Expert</a></li>
+                        <li><a href="/debutant" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Débutant</a></li>
+                        <li><a href="/intermediaire" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Intermédiaire</a></li>
+                        <li><a href="/avance" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Avancé</a></li>
+                        <li><a href="/expert" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Expert</a></li>
                     </ul>
                 </div>
 
                 <div class="flex flex-col gap-2">
                     <h5 class="text-xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Communauté</h5>
                     <ul class="space-y-2">
-                        <li><a href="/github" class="text-white no-underline hover:underline">GitHub</a></li>
-                        <li><a href="/discord" class="text-white no-underline hover:underline">Discord</a></li>
-                        <li><a href="/contribuer" class="text-white no-underline hover:underline">Contribuer</a></li>
-                        <li><a href="/faq" class="text-white no-underline hover:underline">FAQ</a></li>
+                        <li><a href="/github" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">GitHub</a></li>
+                        <li><a href="/discord" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Discord</a></li>
+                        <li><a href="/contribute" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Contribuer</a></li>
+                        <li><a href="/faq" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">FAQ</a></li>
                     </ul>
                 </div>
 
                 <div class="flex flex-col gap-2">
-                    <h5 class="text-xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Légal</h5>
+                    <h5 class="text-xl font-bold bg-gradient-to-l from-[#A5A6FF] to-[#6264F0] bg-clip-text text-transparent">Liens</h5>
                     <ul class="space-y-2">
-                        <li><a href="/mentions-legales" class="text-white no-underline hover:underline">Mentions légales</a></li>
-                        <li><a href="/politique-de-confidentialite" class="text-white no-underline hover:underline">Politique de confidentialité</a></li>
-                        <li><a href="/conditions-d’utilisation" class="text-white no-underline hover:underline">Conditions d’utilisation</a></li>
-                        <li><a href="/cookies" class="text-white no-underline hover:underline">Cookies</a></li>
+                        <li><a href="/legal" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Mentions légales</a></li>
+                        <li><a href="/terms" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Conditions d'utilisation</a></li>
+                        <li><a href="/privacy" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Politique de confidentialité</a></li>
+                        <li><a href="/cookies" class="hover:text-transparent hover:bg-gradient-to-l hover:from-[#A5A6FF] hover:to-[#6264F0] hover:bg-clip-text hover:underline">Cookies</a></li>
                     </ul>
                 </div>
             </div>
@@ -85,6 +87,7 @@
         </div>
     </div>
 </footer>
+</div>
 
 <script src="/js/app.js"></script>
 
